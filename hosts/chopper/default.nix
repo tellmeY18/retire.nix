ac219ca4ebc1d3bd2553e8ec232db19e9576e939
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2025-07-08 18:56:27 +0530

    fixed next

--- hosts/chopper/default.nix
+++ hosts/chopper/default.nix
@@ -71,6 +71,47 @@ in
     lidSwitch = "ignore";
     lidSwitchExternalPower = "ignore";
   };
+  services.nextcloud = {
+    enable = true;
+    hostName = "next.tellmey.tech";
+
+    # Manually increment with every major upgrade.
+    package = pkgs.nextcloud31;
+
+    # Let NixOS install and configure the database automatically.
+    database.createLocally = true;
+
+    # Let NixOS install and configure Redis caching automatically.
+    configureRedis = true;
+
+    # Increase the maximum file upload size to avoid problems uploading videos.
+    maxUploadSize = "16G";
+
+    https = true;
+
+    autoUpdateApps.enable = true;
+
+    extraAppsEnable = true;
+
+    extraApps = with config.services.nextcloud.package.packages.apps; {
+      tasks = tasks;
+      contacts = contacts;
+      memories = memories;
+    };
+
+    settings = {
+      overwriteprotocol = "https";
+      default_phone_region = "IN";
+      port = "8789";
+    };
+
+    # Additional Nextcloud configuration
+    config = {
+      dbtype = "pgsql";
+      adminuser = "admin";
+      adminpassFile = "/home/vysakh/entepass";
+    };
+  };
 
   ####################
   # User Programs    #
