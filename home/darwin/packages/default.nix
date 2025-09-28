51054890d99e1912642f35b92d5f163beb1b4cb3
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2025-09-28 17:03:57 +0530

    fix mac conditioning

--- home/darwin/packages/default.nix
+++ home/darwin/packages/default.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [
     mas
     zoxide
     (google-cloud-sdk.withExtraComponents [
