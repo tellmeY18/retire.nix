4d858769555493183e5c53f80fa1f07cf905bbb2
satanvysakh <satanvysakh@riseup.net>
2026-07-13 01:20:55 +0530

    Fix profiles/base.nix: use optionalAttrs instead of mkIf for security.sudo (darwin compat)

--- profiles/base.nix
+++ profiles/base.nix
@@ -18,7 +18,7 @@
 
   # Require password for sudo. If passwordless is needed for automation,
   # use a targeted sudoers rule instead of blanket NOPASSWD.
-  security.sudo = lib.mkIf pkgs.stdenv.isLinux {
+  security.sudo = lib.optionalAttrs pkgs.stdenv.isLinux {
     enable = true;
     wheelNeedsPassword = true;
   };
